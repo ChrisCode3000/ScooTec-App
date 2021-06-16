1cae6d93ffd3d5296039dd13f66585ec1fa1bf5b
ChrisCode3000 <78434975+ChrisCode3000@users.noreply.github.com>
2021-06-16 11:40:56 +0200

    Update gui_test.py
    I made a GUI with a working button

--- gui_test.py
+++ gui_test.py
@@ -4,11 +4,22 @@ from tkinter import *
 #Setting up the GUI
 root = Tk()
 
-#Creating a label widget
-myLabel = Label(root, text="Hello World!")
+#Creating a function that is executed when clicking the button
+def myClick():
+    myLabel = Label(root, text="You have succesfully clicked the button!").grid(row=3, column=1)
 
-#Showing it onto the screen
-myLabel.pack()
+#Creating a label widget
+#   'padx' and 'pady' for button size, 'command=' for running the function
+myLabel1 = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="This is a GUI Programm.")
+myButton1 = Button(root, text="This is a Button.", padx=50, pady=30, command=myClick)
+
+#Showing it onto the screen   
+#  .grid(row=0, column=0) for position .pack() for none          
+
+myLabel1.grid(row=0, column=0) 
+myLabel2.grid(row=0, column=2)
+myButton1.grid(row=2, column=1)
 
 #Making the programm main loop until closed
 root.mainloop()
