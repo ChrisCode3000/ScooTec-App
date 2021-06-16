ce974a926b3c256464b4798192024bf08b0b1a22
ChrisCode3000 <78434975+ChrisCode3000@users.noreply.github.com>
2021-06-16 10:42:59 +0200

    Update gui_test.py

--- gui_test.py
+++ gui_test.py
@@ -1,12 +1,15 @@
+#Importing Pythons GUI library
 from tkinter import *
 
+#Setting up the GUI
 root = Tk()
 
-#Creating a Label Widget
+#Creating a label widget
 myLabel = Label(root, text="Hello World!")
 
 #Showing it onto the screen
 myLabel.pack()
 
+#Making the programm main loop until closed
 root.mainloop()
 
