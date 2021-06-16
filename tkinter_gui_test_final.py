db8e7f4bbaf01295c8f2e4efd2340b52a0bd7728
ChrisCode3000 <78434975+ChrisCode3000@users.noreply.github.com>
2021-06-16 15:33:00 +0200

    Update tkinter_gui_test_final.py
    The output text error was fixed.

--- tkinter_gui_test_final.py
+++ tkinter_gui_test_final.py
@@ -4,6 +4,10 @@ from tkinter import *
 #Setting up the GUI
 root = Tk()
 root.title("Simple GUI") #GUI window title
+root.geometry("520x200") #GUI window size
+
+myLabel4 = Label(root)
+
 
 #Creating an input box
 myInput1 = Entry(root, width=50, borderwidth=5)
@@ -11,10 +15,14 @@ myInput1.insert(0, "Enter A Text Here") #Predefine an input
 
 #Creating a function that is executed when clicking the button
 def myClick():
-    myLabel3 = Label(root, text="You have succesfully clicked the button!").grid(row=4, column=1)
+    global myLabel4
+    myLabel4.destroy()  #Delete the old label text before adding a new one 
     
-    myOutput1 = "Your input was: " + myInput1.get()
-    myLabel4 = Label(root, text=myOutput1).grid(row=5, column=1)   #Output the input with button click
+    myLabel3 = Label(root, text="You have succesfully clicked the button!").grid(row=4, column=1)
+
+    Output = "Your input was: " + myInput1.get()
+    myLabel4 = Label(root, text=Output)   #Output the input with button click
+    myLabel4.grid(row=5, column=1)
 
 #Creating a label widget
 #   'padx' and 'pady' for button size, 'command=' for running the function, 'fg=' for forground color, 'bg=' for background color
